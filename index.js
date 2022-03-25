b5447c7611e274f5598e9c65fc92dfa19907000a
Francisco Mesar <fmesar87@gmail.com>
2022-03-25 13:31:29 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // code your solution here
+function saturdayFun(argument){
+    if(argument === undefined)
+    {
+        return "This Saturday, I want to roller-skate!" 
+    } else {
+        return "This Saturday, I want to bathe my dog!"
+    }
+}
+
+
+function mondayWork(argument){
+    if(argument === undefined )
+    {
+        return "This Monday, I will go to the office."
+    } else if (argument === "work from home")
+    {
+        return "This Monday, I will work from home."
+    }
+}
+const wrapAdjective = function(argument){
+    if()
+}
+let emphatic = result("a hard worker")
+
+// let result = wrapAdjective('*')
+// let emphatic = result("a hard worker")
+// expect(emphatic).to.equal("You are *a hard worker*!")
+
+    // const part1 = "you are " + argument
+    // if(argument === '*'){
+
+    //    return wrapAdjective2(argument)(`You are ${argument}a hard worker!${argument}`);
+        
